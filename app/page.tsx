56ae008def2bd037e985da93c6b591a93b32e896
Hamed Bahram <hamed.bahram@gmail.com>
2024-01-05 06:20:10 -0500

    Add next-video

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+import Video from 'next-video'
+import sampleVideo from '@/videos/sample-video.mp4'
+
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Next Video Component</h1>
+        <h1 className='text-3xl font-bold text-purple-600'>
+          Next video component
+        </h1>
+
+        <div className='mt-8'>
+          <Video
+            src={sampleVideo}
+            accentColor='#9333ea'
+            className='max-w-[50vw]'
+          />
+        </div>
       </div>
     </section>
   )
